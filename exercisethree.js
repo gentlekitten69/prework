0eb78d8d057c8ceda10979223d184c4b2f9165c1
gentlekitten69 <tiahalibel2010@hotmail.com>
2023-08-06 08:08:48 -0400

    Create exercisethree.js

---
+++ exercisethree.js
@@ -0,0 +1,11 @@
+//Ex. 3.1
+console.log("This is a string, it has quotes.");
+console.log(100);
+console.log("100");
+//Ex. 3.2
+console.log("My name is: William ");
+console.log(30);
+console.log("My age is: 30");
+console.log("Learning new exercises");
+console.log("My zip code is 33410")
+console.log("Coding is a fun experience.")
