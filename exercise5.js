27a9a0341a833c391fa125b6f1f3697604134a55
gentlekitten69 <tiahalibel2010@hotmail.com>
2023-08-06 21:47:09 -0400

    exercise 5

---
+++ exercise5.js
@@ -0,0 +1,13 @@
+// Ex. 5.1
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+
+// Ex. 5.2
+console.log(5 == 5); // ==, true
+console.log(2 > 5); // >, false
+console.log(8 <= 7); // <=, false
+console.log(9 != 8); // !=, true
