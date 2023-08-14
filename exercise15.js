c093df12b8676c2b8215e6724381db8c68f0ad8a
gentlekitten69 <tiahalibel2010@hotmail.com>
2023-08-13 20:57:42 -0400

    exercise 15

---
+++ exercise15.js
@@ -0,0 +1,50 @@
+//Ex 15.1
+//time to greet guests 1-10!
+
+console.log(`Hello, Guest Number ${1}`);
+console.log(`Hello, Guest Number ${2}`);
+console.log(`Hello, Guest Number ${3}`);
+console.log(`Hello, Guest Number ${4}`);
+console.log(`Hello, Guest Number ${5}`);
+console.log(`Hello, Guest Number ${6}`);
+console.log(`Hello, Guest Number ${7}`);
+console.log(`Hello, Guest Number ${8}`);
+console.log(`Hello, Guest Number ${9}`);
+console.log(`Hello, Guest Number ${10}`);
+
+const drinksMenu = ["soda", "tea", "coffee", "juice", "milk", "water"]
+console.log("Welcome in! Here are your drink options:");
+for (let i in drinksMenu) {
+    console.log(drinksMenu[i]);
+}
+
+
+console.log("Welcome in! Here are your drink options:");
+
+console.log(drinksMenu[0]);
+console.log(drinksMenu[1]);
+console.log(drinksMenu[2]);
+console.log(drinksMenu[3]);
+console.log(drinksMenu[4]);
+console.log(drinksMenu[5]);
+
+//Ex 15.2
+/*for (let i = 0; i <= 5; i++) {
+    console.log(`Now we're on number ${i}`);
+}
+for (let i = 0; i < 10; i++) {
+    console.log("This is going to be logged 10 times.");
+    console.log("And it's the same every time.");   
+}*/
+
+//Ex 15.3
+//Write a loop that displays the square and the cube of each integer from 0 up to 5, inclusive
+//Remember the exponentiation operator **
+
+for (i = 0 /* your code here */; i <= 5/* your code here */; i++) {
+    let square = i ** 2;
+    let cube = i*i*i  //your code here
+
+    console.log(`The square of ${i} is ${square}` /* your code here */);
+    console.log(`The cube of ${i} is ${cube}`)//your code here for second console.log() statement
+}
